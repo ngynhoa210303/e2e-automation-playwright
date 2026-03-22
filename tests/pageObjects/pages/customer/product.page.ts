b6ba7c1008cd543751f042636e4cbd2f68a44f49
Hoang Thi Thu Huyen <huyenhtt1.bhsoft@gmail.com>
2026-03-22 14:32:45 +0700

    filter_product

--- tests/pageObjects/pages/customer/product.page.ts
+++ tests/pageObjects/pages/customer/product.page.ts
@@ -1,14 +1,53 @@
-import { BasePage } from '../../base.page';
+import { BasePage } from "../../base.page";
 
 export default class ProductCustomerPage extends BasePage {
   readonly select_category = this.page.locator('[name="category"]');
-  readonly products_card = this.page.locator('//div[contains(@class,"p-4 flex")]');
+  readonly products_card = this.page.locator(
+    '//div[contains(@class,"p-4 flex")]',
+  );
+  readonly select_color = this.page.locator('[name="color"]');
+  readonly select_price = this.page.locator('[name="price"]');
+  readonly select_brand = this.page.locator('[name="brand"]');
+  readonly select_size = this.page.locator('[name="size"]');
+  readonly select_collar = this.page.locator('[name="collar"]');
+  readonly select_sleeve = this.page.locator('[name="sleeve"]');
+  readonly minPrice = this.page.locator('input[placeholder="Từ"]');
+  readonly maxPrice = this.page.locator('input[placeholder="Đến"]');
+  readonly resetButton = this.page.locator('button:has-text("Đặt lại")');
 
+  async clickResetButton() {
+    await this.resetButton.click();
+  }
   async searchProduct(searchTerm: string) {
     await this.navBar.searchInput.fill(searchTerm);
   }
+  async selectCategory(category: string) {
+    await this.select_category.selectOption({ label: category });
+  }
+  async selectBrand(value: string) {
+    await this.select_brand.selectOption({ label: value });
+  }
+  async selectColor(value: string) {
+    await this.select_color.selectOption({ label: value });
+  }
+  async selectSize(value: string) {
+    await this.select_size.selectOption({ label: value });
+  }
+  async selectSleeve(value: string) {
+    await this.select_sleeve.selectOption({ label: value });
+  }
+  async selectCollar(value: string) {
+    await this.select_collar.selectOption({ label: value });
+  }
+  async filterByPrice(min: string, max: string) {
+    await this.minPrice.waitFor({ state: 'visible' });
+    await this.minPrice.fill(min);
+    await this.maxPrice.waitFor({ state: 'visible' });
+    await this.maxPrice.fill(max);
+    await this.page.keyboard.press("Enter");
+  }
 
   async open() {
-    await super.open('/products');
+    await super.open("/products");
   }
 }
--- tests/specs/customer-specs/products.spec.ts
+++ tests/specs/customer-specs/products.spec.ts
@@ -1,8 +1,8 @@
-import { test, expect } from '../../pageObjects/pageFixture';
-import dotenv from 'dotenv';
+import { test, expect } from "../../pageObjects/pageFixture";
+import dotenv from "dotenv";
 dotenv.config();
-import filterData from '../../../util/data.json';
-test.describe('Product Filter', () => {
+import filterData from "../../../util/data.json";
+test.describe("Product Filter", () => {
   test.beforeEach(async ({ loginPage }) => {
     await loginPage.open();
     await loginPage.login(
@@ -17,28 +17,181 @@ test.describe('Product Filter', () => {
     await loginPage.page.waitForTimeout(1000);
   });
 
-  test('TC01 - Filter products by name', async ({ productCustomerPage }) => {
-    await test.step('Open products page', async () => {
+  test("TC01 - Filter products by name", async ({ productCustomerPage }) => {
+    await test.step("Open products page", async () => {
       await productCustomerPage.waitForPageLoad();
       const beforeCount = await productCustomerPage.products_card.count();
       expect(beforeCount).toBeGreaterThan(1);
       const typeSearch = filterData.filters.find(
-        (f) => f.type === 'product-name',
+        (f) => f.type === "product-name",
       );
-      await productCustomerPage.searchProduct(typeSearch?.value || '');
+      await productCustomerPage.searchProduct(typeSearch?.value || "");
       await productCustomerPage.page.waitForTimeout(500);
       await productCustomerPage.page
-        .locator('p')
+        .locator("p")
         .filter({ hasText: typeSearch?.value })
         .first()
         .click();
       await productCustomerPage.page.waitForTimeout(500);
       await productCustomerPage.page
-        .locator('h1')
+        .locator("h1")
         .filter({ hasText: typeSearch?.value })
         .waitFor({
-          state: 'visible',
+          state: "visible",
         });
+    });
+  });
+  test("TC02 - Filter products by more options", async ({
+    productCustomerPage,
+  }) => {
+    const getFilter = (type: string) => {
+      const f = filterData.filters.find((f) => f.type === type);
+      if (!f) throw new Error(`Missing filter: ${type}`);
+      return f;
+    };
+    await test.step("Filter by category", async () => {
+      const beforeCount = await productCustomerPage.products_card.count();
+
+      const filter = getFilter("category");
+
+      await productCustomerPage.selectCategory(filter.value);
+      await productCustomerPage.waitForPageLoad();
+
+      const afterCount = await productCustomerPage.products_card.count();
+
+      expect(afterCount).toBeGreaterThan(0);
+      expect(afterCount).toBeLessThanOrEqual(beforeCount);
+    });
+    await test.step("Filter by prices", async () => {
+      await expect(productCustomerPage.minPrice).toBeVisible();
+      const filter = getFilter("price");
+      const [min, max] = filter.value.split("-");
+
+      await productCustomerPage.filterByPrice(min, max);
+
+      await expect(productCustomerPage.products_card.first()).toBeVisible();
+
+      const priceTexts = await productCustomerPage.products_card
+        .locator(".product-price")
+        .allInnerTexts();
+
+      for (const text of priceTexts) {
+        const price = parseInt(text.replace(/\D/g, ""));
+
+        expect(price).toBeGreaterThanOrEqual(Number(min));
+        expect(price).toBeLessThanOrEqual(Number(max));
+      }
+    });
+
+    await test.step("Filter by brand", async () => {
+      const beforeCount = await productCustomerPage.products_card.count();
+
+      const filter = getFilter("brand");
+
+      await productCustomerPage.selectBrand(filter.value);
+      await productCustomerPage.waitForPageLoad();
+
+      const afterCount = await productCustomerPage.products_card.count();
+
+      expect(afterCount).toBeGreaterThan(0);
+      expect(afterCount).toBeLessThanOrEqual(beforeCount);
+    });
+    await test.step("Filter by color", async () => {
+      const beforeCount = await productCustomerPage.products_card.count();
+
+      const filter = getFilter("color");
+
+      await productCustomerPage.selectColor(filter.value);
+      await productCustomerPage.waitForPageLoad();
+
+      const afterCount = await productCustomerPage.products_card.count();
+
+      expect(afterCount).toBeGreaterThan(0);
+      expect(afterCount).toBeLessThanOrEqual(beforeCount);
+    });
+    await test.step("Filter by size", async () => {
+      const beforeCount = await productCustomerPage.products_card.count();
+
+      const filter = getFilter("size");
+
+      await productCustomerPage.selectSize(filter.value);
+      await productCustomerPage.waitForPageLoad();
+
+      const afterCount = await productCustomerPage.products_card.count();
+
+      expect(afterCount).toBeGreaterThan(0);
+      expect(afterCount).toBeLessThanOrEqual(beforeCount);
+    });
+    await test.step("Filter by collar style", async () => {
+      const beforeCount = await productCustomerPage.products_card.count();
+
+      const filter = getFilter("collar style");
+
+      await productCustomerPage.selectCollar(filter.value);
+      await productCustomerPage.waitForPageLoad();
+
+      const afterCount = await productCustomerPage.products_card.count();
+
+      expect(afterCount).toBeGreaterThan(0);
+      expect(afterCount).toBeLessThanOrEqual(beforeCount);
+    });
+    await test.step("Filter by sleeve", async () => {
+      const beforeCount = await productCustomerPage.products_card.count();
+
+      const filter = getFilter("sleeve");
+
+      await productCustomerPage.selectSleeve(filter.value);
+      await productCustomerPage.waitForPageLoad();
+
+      const afterCount = await productCustomerPage.products_card.count();
+
+      expect(afterCount).toBeGreaterThan(0);
+      expect(afterCount).toBeLessThanOrEqual(beforeCount);
+    });
+  });
+  test("TC03 - Reset all filters", async ({ productCustomerPage }) => {
+    const getFilter = (type: string) => {
+      const f = filterData.filters.find((f) => f.type === type);
+      if (!f) throw new Error(`Missing filter: ${type}`);
+      return f;
+    };
+
+     await test.step("Apply filters", async () => {
+      const category = getFilter("category");
+      const price = getFilter("price");
+
+      const [min, max] = price.value.split("-");
+
+      await productCustomerPage.selectCategory(category.value);
+      await productCustomerPage.filterByPrice(min, max);
+
+      await productCustomerPage.waitForPageLoad();
+    });
+
+    let filteredCount = 0;
+
+    await test.step("Get filtered count", async () => {
+      filteredCount = await productCustomerPage.products_card.count();
+      expect(filteredCount).toBeGreaterThan(0);
+    });
+
+    await test.step("Click reset", async () => {
+      await productCustomerPage.clickResetButton();
+    });
+
+    await expect(productCustomerPage.products_card.first()).toBeVisible();
+
+    await test.step("Verify UI reset", async () => {
+      await expect(productCustomerPage.minPrice).toHaveValue("");
+      await expect(productCustomerPage.maxPrice).toHaveValue("");
+
+      await expect(productCustomerPage.select_category).toHaveValue("");
+    });
+
+    await test.step("Verify product list reset", async () => {
+      const resetCount = await productCustomerPage.products_card.count();
+
+      expect(resetCount).toBeGreaterThanOrEqual(filteredCount);
     });
   });
   test.afterEach(async ({ homePage }) => {
